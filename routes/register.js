f7cb1d54e98fa1b21a17a9e6e94d5cecfcfeba93
dauqu <sujeetsingh734321@gmail.com>
2022-12-19 14:26:05 +0530

    ch

--- routes/register.js
+++ routes/register.js
@@ -5,8 +5,9 @@ const bcrypt= require("bcrypt")
 
 
 //register user
-router.post("/", validation, async (req, res) => {
+router.post("/", async (req, res) => {
 
+  console.log(req.body);
   // //generate random userid
   const userid = 
     Math.random().toString(36).substring(2, 15) +
@@ -23,8 +24,9 @@ router.post("/", validation, async (req, res) => {
   });
   try {
     await user.save();
-    res.status(200).send({
+    res.status(200).json({
       message: "User created successfully",
+      user
     });
   } catch (error) {
     res.status(400).json({ message: error.message, status: "error" });
